50b76029a0fbe2c57bdc022e3097c3d57e94cb19
MSingh-007 <manujsingh96@gmail.com>
2021-01-31 20:35:30 +0530

    version 1.3

--- DataLibrary/IConsultationDAL.cs
+++ DataLibrary/IConsultationDAL.cs
@@ -7,6 +7,7 @@ namespace DataLibrary
     public interface IConsultationDAL
     {
         Task AddConsultation(ConsultationModel consultation);
+        Task<List<int>> GetConsultationsOnDaysBack(int days);
         Task DeleteConsultation(int consultationId);
         Task EditConsultation(ConsultationModel consultation);
         Task<List<ConsultationModel>> GetConsultations(int patientId);
